a6f203a8bf9afe0425533bd155273049d0273ea6
MarengoSimone <62547054+MarengoSimone@users.noreply.github.com>
2021-09-20 17:47:48 +0200

    Creati DB dell'esercizio 2

---
+++ 02-Aeroporto/02-Aeroporto/CreazioneDB.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE Aeroporto (
+	Citta varchar(30) PRIMARY KEY NOT NULL,
+	Nazione varchar(3) NOT NULL,
+	NumPiste int NOT NULL
+);
+
+CREATE TABLE Volo (
+	IdVolo int PRIMARY KEY NOT NULL IDENTITY,
+	GiornoSett varchar(20) NOT NULL,
+	CittaPart varchar(30) NOT NULL,
+	OraPart date NOT NULL,
+	CittaArr varchar(30) NOT NULL,
+	OraArr date NOT NULL,
+	TipoAereo varchar(50)
+);
+
+CREATE TABLE Aereo (
+	TipoAereo varchar(50) PRIMARY KEY NOT NULL,
+	NumPasseggeri int NOT NULL,
+	QtaMerci int
+);
