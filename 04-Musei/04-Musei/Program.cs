2b0c489cbe1f0b1e0e81a8c1f4f9d06d2f6b96e2
MarengoSimone <62547054+MarengoSimone@users.noreply.github.com>
2021-09-27 18:37:54 +0200

    Aggiunti DB e dati dell'esercizio 4

---
+++ 04-Musei/04-Musei/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _04_Musei
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
