4efceb0d2cdd7bda91c62c2fcf7edeae0683a480
MarengoSimone <62547054+MarengoSimone@users.noreply.github.com>
2021-10-02 13:05:43 +0200

    Primo caricamento esercizio 5

---
+++ 05-Automobili/05-Automobili/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _05_Automobili
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 05-Automobili/Query/createTables.sql
@@ -0,0 +1,37 @@
+﻿CREATE TABLE Automobile(
+	Targa varchar(7) PRIMARY KEY NOT NULL,
+	Marca varchar(20) NOT NULL,
+	Cilindrata int NOT NULL,
+	Potenza int NOT NULL,
+	CodF int NOT NULL,
+	CodAss int NOT NULL,
+	CONSTRAINT [FK_Automobile_ToTable] FOREIGN KEY ([CodF]) REFERENCES [dbo].[Proprietari] ([CodF]),
+    CONSTRAINT [FK_Automobile_ToTable_1] FOREIGN KEY ([CodAss]) REFERENCES [dbo].[Assicurazioni] ([CodAss])
+);
+
+CREATE TABLE Proprietari(
+	CodF int PRIMARY KEY IDENTITY(1,1),
+	Nome varchar(30) NOT NULL,
+	Residenza varchar(50) NOT NULL
+);
+
+CREATE TABLE Assicurazioni(
+	CodAss int PRIMARY KEY IDENTITY(1,1),
+	Nome varchar(30) NOT NULL,
+	Sede varchar(30) NOT NULL
+);
+
+CREATE TABLE Sinistro(
+	CodS int PRIMARY KEY IDENTITY(1,1),
+	Localita varchar(50) NOT NULL,
+	DataSinistro date NOT NULL
+);
+
+CREATE TABLE AutoCoinvolte(
+	CodS int NOT NULL PRIMARY KEY,
+	Targa varchar(7) NOT NULL,
+	ImportoDanno int NOT NULL,
+	CONSTRAINT [FK_AutoCoinvolte_ToTable] FOREIGN KEY ([CodS]) REFERENCES [dbo].[Sinistro] ([CodS]),
+	CONSTRAINT [FK_AutoCoinvolte1_ToTable] FOREIGN KEY ([Targa]) REFERENCES [dbo].[Automobile] ([Targa]),
+);
+
