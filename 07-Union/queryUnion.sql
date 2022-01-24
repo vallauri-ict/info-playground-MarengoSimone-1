7cdd9d210ab8aa53c6445e2cec25bf4df0179c29
MarengoSimone <62547054+MarengoSimone@users.noreply.github.com>
2022-01-24 18:09:39 +0100

    Caricato esercizio 7: Union

---
+++ 07-Union/queryUnion.sql
@@ -0,0 +1,13 @@
+﻿SELECT 'Internet' as Tipo,Data
+FROM VenditeInternet
+UNION
+SELECT 'Negozio' as Tipo,Data 
+FROM VenditeNegozio
+ORDER BY Data Desc
+
+SELECT Data
+FROM VenditeInternet
+INTERSECT
+SELECT Data 
+FROM VenditeNegozio
+ORDER BY Data Desc
