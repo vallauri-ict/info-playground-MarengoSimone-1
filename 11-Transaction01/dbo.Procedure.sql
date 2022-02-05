058d30a0a140e92921133a9587a2e035a39ac400
MarengoSimone <62547054+MarengoSimone@users.noreply.github.com>
2022-02-05 12:08:00 +0100

    Caricato esercizio 11: Transaction[01]

---
+++ 11-Transaction01/dbo.Procedure.sql
@@ -0,0 +1,31 @@
+﻿CREATE PROCEDURE [dbo].[Bonifico]
+	@importo float = 0,
+	@contoSorgente int,
+	@contoDestinazione int
+AS
+	if(@contoDestinazione=@contoSorgente)
+		return -2;
+
+		BEGIN TRANSACTION
+		INSERT INTO Movimenti(importo,data,tipo,codConto)
+		VALUES(@importo,GETDATE(),'P',@contoSorgente)
+
+		if(@@ERROR<>0)
+		BEGIN
+			ROLLBACK
+			RAISERROR('Errore durante il prelievo',16,1)
+			return -1;
+		END
+
+		INSERT INTO Movimenti(importo,data,tipo,codConto)
+		VALUES(@importo,GETDATE(),'V',@contoDestinazione)
+
+		if(@@ERROR<>0)
+		BEGIN
+			ROLLBACK
+			RAISERROR('Errore durante il versamento',16,2)
+			return -1;
+		END
+		-- STEP 4: If we reach this point, the commands completed successfully
+	COMMIT
+RETURN 0
---
+++ 11-Transaction01/queryBonifico.sql
@@ -0,0 +1,2 @@
+﻿EXEC dbo.Bonifico 1000,1,2
+EXEC dbo.Bonifico 3000,1,1
