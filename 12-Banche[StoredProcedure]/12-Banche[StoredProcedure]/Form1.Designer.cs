c986d298d8a4808b66b7fb26393921db9b76ff71
MarengoSimone <62547054+MarengoSimone@users.noreply.github.com>
2022-02-11 15:56:08 +0100

    Proseguito esercizio 12

--- 12-Banche[StoredProcedure]/12-Banche[StoredProcedure]/Form1.Designer.cs
+++ 12-Banche[StoredProcedure]/12-Banche[StoredProcedure]/Form1.Designer.cs
@@ -28,13 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBanca = new System.Windows.Forms.TextBox();
+            this.txtComune = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSP1 = new System.Windows.Forms.Button();
+            this.btnSP2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Banca:";
+            // 
+            // txtBanca
+            // 
+            this.txtBanca.Location = new System.Drawing.Point(113, 38);
+            this.txtBanca.Name = "txtBanca";
+            this.txtBanca.Size = new System.Drawing.Size(130, 22);
+            this.txtBanca.TabIndex = 1;
+            // 
+            // txtComune
+            // 
+            this.txtComune.Location = new System.Drawing.Point(418, 38);
+            this.txtComune.Name = "txtComune";
+            this.txtComune.Size = new System.Drawing.Size(130, 22);
+            this.txtComune.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(335, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Comune:";
+            // 
+            // btnSP1
+            // 
+            this.btnSP1.Location = new System.Drawing.Point(44, 147);
+            this.btnSP1.Name = "btnSP1";
+            this.btnSP1.Size = new System.Drawing.Size(199, 41);
+            this.btnSP1.TabIndex = 4;
+            this.btnSP1.Text = "Numero Filiali";
+            this.btnSP1.UseVisualStyleBackColor = true;
+            this.btnSP1.Click += new System.EventHandler(this.btnSP1_Click);
+            // 
+            // btnSP2
+            // 
+            this.btnSP2.Location = new System.Drawing.Point(349, 147);
+            this.btnSP2.Name = "btnSP2";
+            this.btnSP2.Size = new System.Drawing.Size(199, 41);
+            this.btnSP2.TabIndex = 5;
+            this.btnSP2.Text = "Elenco Filiali";
+            this.btnSP2.UseVisualStyleBackColor = true;
+            this.btnSP2.Click += new System.EventHandler(this.btnSP2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(647, 246);
+            this.Controls.Add(this.btnSP2);
+            this.Controls.Add(this.btnSP1);
+            this.Controls.Add(this.txtComune);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtBanca);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBanca;
+        private System.Windows.Forms.TextBox txtComune;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSP1;
+        private System.Windows.Forms.Button btnSP2;
     }
 }
 
--- 12-Banche[StoredProcedure]/12-Banche[StoredProcedure]/Form1.cs
+++ 12-Banche[StoredProcedure]/12-Banche[StoredProcedure]/Form1.cs
@@ -1,20 +1,85 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace _12_Banche_StoredProcedure_
 {
     public partial class Form1 : Form
     {
+        private static readonly string CONNECTION_STRING = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\gsalt\Desktop\info-playground\12-Banche[StoredProcedure]\dbBanche.mdf;Integrated Security=True;Connect Timeout=30";
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnSP1_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(CONNECTION_STRING))
+            {
+                con.Open();
+                string sql = "[Procedure]";
+
+                using (SqlCommand cmd = new SqlCommand(sql, con))
+                {
+                    SqlParameter data = new SqlParameter();
+                    //data.ParameterName = "numero";
+
+                    //data.Value = numero;
+                    data.Direction = ParameterDirection.Input;
+                    data.DbType = DbType.Int32;
+                    cmd.Parameters.Add(data);
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        string s = "";
+
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            s += reader.GetValue(i) + " ";
+                        }
+                    }
+                }
+            }
+        }
+
+        private void btnSP2_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(CONNECTION_STRING))
+            {
+                con.Open();
+                string sql = "[Visulizza]";
+
+                using (SqlCommand cmd = new SqlCommand(sql, con))
+                {
+                    SqlParameter data = new SqlParameter();
+                    //data.ParameterName = "comune";
+
+                    //data.Value = v;
+                    data.Direction = ParameterDirection.Input;
+                    data.DbType = DbType.String;
+                    cmd.Parameters.Add(data);
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    string s = "";
+                    while (reader.Read())
+                    {
+
+
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            s += reader.GetValue(i) + " ";
+                        }
+                    }
+                    Console.WriteLine(s);
+                }
+            }
         }
     }
 }
