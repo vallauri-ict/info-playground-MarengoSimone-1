0c714cfba63b0b767df1d93b872cebe63b10a4b7
MarengoSimone <62547054+MarengoSimone@users.noreply.github.com>
2022-04-04 12:30:14 +0200

    Caricato esercizio 15: Maturità 2017

---
+++ 15-Maturità 2017/createTables.sql
@@ -0,0 +1,58 @@
+﻿CREATE TABLE Passeggeri(
+	Id_Utente int primary key not null identity(1,1),
+	nome varchar(20) not null,
+	cognome varchar(20) not null,
+	telefono varchar(10) not null,
+	email varchar(40) not null,
+	docIdentita varchar(10) not null
+)
+
+CREATE TABLE Autisti(
+	id_Autista int primary key not null identity(1,1),
+	nome varchar(20) not null,
+	cognome varchar(20) not null,
+	telefono varchar(10) not null,
+	email varchar(40) not null,
+	fotografia varchar(10) not null,
+	datiAuto varchar(50) not null,
+	scadPatente date not null
+)
+
+CREATE TABLE Viaggio(
+	id_Viaggio int primary key not null identity(1,1),
+	cittaPartenza varchar(25) not null,
+	cittaArrivo varchar(25) not null,
+	tempiStimati int not null,
+	prezzo decimal(3,2) not null,
+	costo decimal(3,2) not null,
+	[data] date not null,
+	ora time not null,
+	soste int not null,
+	bagagli bit not null,
+	animali bit not null,
+	isPrenotabile bit not null,
+	IdAutista int foreign key references Autisti(id_Autista) not null
+)
+
+CREATE TABLE Prenotazioni(
+	id_Viaggio int foreign key references Viaggio(id_Viaggio) not null,
+	Id_Utente int foreign key references Passeggeri(Id_Utente) not null,
+	stato bit not null,
+	primary key(id_Viaggio, Id_Utente)
+)
+
+CREATE TABLE FeedbackAutista(
+	id_FeedbackAutista int primary key identity(1,1) not null,
+	voto int not null,
+	descrizione varchar(100),
+	idViaggio int foreign key references Viaggio(id_Viaggio) not null,
+	idUtente int foreign key references Passeggeri(Id_Utente) not null
+)
+
+CREATE TABLE FeedbackPasseggero(
+	id_FeedbackPasseggero int primary key identity(1,1) not null,
+	voto int not null,
+	descrizione varchar(100),
+	idViaggio int foreign key references Viaggio(id_Viaggio) not null,
+	idAutisti int foreign key references Autisti(id_Autista) not null
+)
---
+++ 15-Maturità 2017/queryEsame.sql
@@ -0,0 +1,24 @@
+﻿/* Query 1 */
+SELECT a.nome, v.costo, a.datiAuto, v.ora
+FROM Viaggio v, Autisti a
+WHERE v.IdAutista = a.id_Autista
+AND v.cittaArrivo='Alba'
+AND v.cittaPartenza='Savigliano'
+AND v.data = '2022-04-02'
+AND v.isPrenotabile = 1
+order by v.ora
+
+/* Query 2 */
+SELECT pas.email, pas.cognome, pas.nome
+FROM Prenotazioni p, Passeggeri pas
+WHERE p.stato = 1
+AND p.Id_Utente = pas.Id_Utente
+
+/* Query 3 */
+SELECT *
+FROM Passeggeri p, FeedbackAutista fp, Viaggio v, Prenotazioni pren
+WHERE p.Id_Utente = fp.idUtente
+AND v.id_Viaggio = pren.id_Viaggio
+AND pren.Id_Utente = p.Id_Utente
+AND v.id_Viaggio = 1
+AND 1 < (SELECT AVG(fa.voto) FROM FeedbackAutista fa WHERE fa.idUtente = p.Id_Utente)
